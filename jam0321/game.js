11b476de82888dd86aef1f2ca709f32bfdeece56
福田 <zionbanzai3@gmail.com>
2017-03-22 00:23:20 +0900

    no message

--- jam0321/game.js
+++ jam0321/game.js
@@ -26,12 +26,12 @@ const pick = [
 ];
 const main = _ => {
     ctx.clearRect(0, 0, 800, 600);
-    ctx.fillText("Press space to start", 0, 20);
-    if(end == 2000) {
+    if(end === 2000) {
         ctx.clearRect(0, 0, 800, 600);
         pick.push("");
         pick.push("your score:" + localStorage.getItem('count'));
     } else if(end < 2000) {
+        ctx.fillText("Press space to start", 0, 20);
         for(let i = 0; i < pick.length && i * 100 < end; i += 1) {
             ctx.fillText(pick[i].substr(0, end - i * 100), 0, 50 + i * 30);
         }
@@ -39,13 +39,13 @@ const main = _ => {
             end = 1;
             input.space = false;
             makeWindow(Math.random() * 1000, Math.random() * 600);
-            localStorage.setItem('count', parseInt(localStorage.getItem('count')) + 1);
+            //localStorage.setItem('count', parseInt(localStorage.getItem('count')));
         }
-    } else if(end > 2020) {
-        let result = "Score:" + localStorage.getItem('count') + " https://hukuda222.github.io/gamejam/jam0321/ ";
+    } else if(end === 2020) {
+        let result = "とにかくキーを叩くだけのゲーム。 Score:" + localStorage.getItem('count') + " https://hukuda222.github.io/gamejam/jam0321/ ";
         location.href = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(result) + "&hashtags=traP3jam";
     }
-    if(end > 0) end++;
+    if(end > 0 && end < 3000) end++;
 };
 const init = _ => {
     let canvas = document.getElementById("cv");
