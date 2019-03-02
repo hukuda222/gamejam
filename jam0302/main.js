5686be3bea6c21d9dcb2b6c1822a9a50bb826bac
福田 <zionbanzai3@gmail.com>
2019-03-02 19:00:18 +0900

    jam0302

---
+++ jam0302/main.js
@@ -0,0 +1,252 @@
+/* global phina GameApp */
+
+phina.globalize();
+
+const SCREEN_WIDTH = 600;
+const SCREEN_HEIGHT = 480;
+
+const is_cross = (a, b, c, d) => {
+  const a0 = (b.y - a.y) / (b.x - a.x);
+  const a1 = (d.y - c.y) / (d.x - c.x);
+
+  const x = (a0 * a.x - a.y - a1 * c.x + c.y) / (a0 - a1);
+  const y = (b.y - a.y) / (b.x - a.x) * (x - a.x) + a.y;
+  if (Math.abs(a0) === Math.abs(a1)) return false;
+
+  if (x > Math.max(a.x, b.x) || x > Math.max(c.x, d.x) ||
+             y > Math.max(a.y, b.y) || y > Math.max(c.y, d.y) ||
+                     x < Math.min(a.x, b.x) || x < Math.min(c.x, d.x) ||
+                             x < Math.min(a.x, b.x) || y < Math.min(c.y, d.y)) return false;
+
+  return { x: x, y: y };
+};
+
+const is_in = (vecs, x, y) => {
+  let count = 0;
+  const ten = {x: x, y: y};
+  const po = {x: 114514, y: y};
+  for (let i = 0; i < vecs.length - 1; i++) {
+    if (is_cross(vecs[i], vecs[i + 1], ten, po))count += 1;
+  }
+  return count % 2 === 1;
+};
+
+const ASSETS = {
+  image: {
+    "sakura": "./sakura.png",
+    "kokuhaku": "./kokuhaku.png",
+    "player": "./player.png",
+    "sumaho": "./sumaho.png",
+    "chari": "./chari.png",
+    "gal": "./gal.png"
+  }
+};
+
+const myscenes = [
+  {
+    label: "タイトル",
+    className: "Title",
+    nextLabel: "メイン"
+  },
+  {
+    label: "メイン",
+    className: "Main",
+    nextLabel: "リザルト"
+  },
+  {
+    label: "リザルト",
+    className: "Result",
+    nextLabel: "タイトル"
+  }
+
+];
+
+phina.define("Title", {
+  superClass: "DisplayScene",
+
+  init: function() {
+    this.superInit();
+    this.backgroundColor = "pink";
+    const label = Label({
+      text: "ときどきメモリアル",
+      fontSize: 50,
+      fill: "white"
+    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(1));
+    const label2 = Label({
+      text: "君は主人公の親友だ。\n桜の樹の下の告白を成功させよう。\n十字キーで移動し、" +
+      "\n四方から来る人をせき止めるんだ！\n\n\nEnter to start",
+      fontSize: 30,
+      fill: "white"
+    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(5));
+  }, // タッチ開始
+  update: function(app) {
+    if (app.keyboard.getKey("enter")) {
+      this.exit();
+    }
+  }
+});
+
+phina.define("Result", {
+  superClass: "DisplayScene",
+
+  init: function(param) {
+    this.superInit();
+    this.score = param.score;
+    this.backgroundColor = "pink";
+
+    Label({
+      text: "せき止めたのは" + (param.score - 1) + "人。\nTキーでツイート",
+      fontSize: 50,
+      fill: "white"
+    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(4));
+  },
+  update: function(app) {
+    if (app.keyboard.getKey("T")) {
+      let result = "せき止めた人数は" + this.score - 1 + "人でした！" + " https://hukuda222.github.io/gamejam/jam0302/ ";
+      location.href = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(result) + "&hashtags=traP3jam";
+    } else if (app.keyboard.getKey("enter")) {
+      this.exit();
+    }
+  }
+});
+
+phina.define("Main", {
+  superClass: "DisplayScene",
+
+  init: function() {
+    this.superInit();
+    this.backgroundColor = "skyblue";
+    this.count = 50;
+
+    const piza = Sprite("sakura").addChildTo(this);
+    piza.x = 300;
+    piza.y = 260;
+    piza.width = 200;
+    piza.height = 200;
+
+    this.kokuhaku = Sprite("kokuhaku").addChildTo(this);
+    this.kokuhaku.x = 300;
+    this.kokuhaku.y = 310;
+    this.kokuhaku.width = 100;
+    this.kokuhaku.height = 150;
+
+    this.group = DisplayElement().addChildTo(this);
+
+    const player = Sprite("player").addChildTo(this.group);
+    player.x = 30;
+    player.y = 30;
+    player.width = 50;
+    player.height = 75;
+
+    this.enemys = [];
+  },
+
+  update: function(app) {
+    const keyboard = app.keyboard;
+    // 左右移動
+    if (keyboard.getKey("left")) {
+      this.group.children.each((g) => {
+        g.x -= 20;
+      });
+    }
+    if (keyboard.getKey("right")) {
+      this.group.children.each((g) => {
+        g.x += 20;
+      });
+    }
+    // 上下移動
+    if (keyboard.getKey("up")) {
+      this.group.children.each((g) => {
+        g.y -= 20;
+      });
+    }
+    if (keyboard.getKey("down")) {
+      this.group.children.each((g) => {
+        g.y += 20;
+      });
+    }
+    this.count -= 1;
+    if (this.count === 0) {
+      this.count = 100;
+      let type = "";
+      if (Math.random() > 0.33)type = "sumaho";
+      else if (Math.random() > 0.33)type = "gal";
+      else type = "chari";
+      let e = Sprite(type);
+      e.type = type;
+      if (type != "chari")e.width = 50;
+      else e.width = 75;
+      e.height = 75;
+      e.is_in = false;
+      e.addChildTo(this);
+
+      let x0 = Math.random() * SCREEN_WIDTH;
+      let y0 = Math.random() * SCREEN_HEIGHT;
+      let x1 = Math.random() * SCREEN_WIDTH;
+      let y1 = Math.random() * SCREEN_HEIGHT;
+      if (Math.random() <= 0.25)x0 = 1;
+      else if (Math.random() <= 0.25)x0 = SCREEN_WIDTH - 1;
+      else if (Math.random() <= 0.25)y0 = 1;
+      else y0 = SCREEN_HEIGHT - 1;
+
+      if (Math.random() <= 0.25)x1 = -1;
+      else if (Math.random() <= 0.25)x1 = SCREEN_WIDTH + 1;
+      else if (Math.random() <= 0.25)y1 = -1;
+      else y1 = SCREEN_HEIGHT + 1;
+
+      if (Math.random() < 0.5) {
+        e.tweener.set({x: x0, y: y0}).to({x: 250 + 100 * Math.random(), y: 260 + 100 * Math.random()},
+          1500 + Math.random() * 1000)
+          .to({x: x1, y: y1}, 1500 + (Math.random() * 1000), "swing").play();
+      } else {
+        e.tweener.set({x: x0, y: y0})
+          .to({x: x1, y: y1}, 2500 + (Math.random() * 1000), "swing").play();
+      }
+      this.enemys.push(e);
+    }
+    this.group.children.each((g) => {
+      this.enemys.forEach((e) => {
+        if (e.hitTestElement(g)) {
+          e.is_in = true;
+          let ne = Sprite(e.type);
+          if (e.type !== "chari")ne.width = 50;
+          else ne.width = 75;
+          ne.height = 75;
+          ne.x = (e.x + g.x) / 2;
+          ne.y = (e.y + g.y) / 2;
+          ne.addChildTo(this.group);
+        }
+      });
+    });
+    this.enemys = this.enemys.filter((e) => {
+      if (e.is_in || e.x < 0 || e.x > SCREEN_WIDTH || e.y < 0 || e.y > SCREEN_HEIGHT) {
+        e.remove();
+        return false;
+      };
+      return true;
+    });
+
+    this.group.children.each((g) => {
+      if (g.hitTestElement(this.kokuhaku)) {
+        this.exit({score: this.group.children.length});
+      }
+    });
+    this.enemys.forEach((e) => {
+      if (e.hitTestElement(this.kokuhaku)) {
+        this.exit({score: this.group.children.length});
+      }
+    });
+  }
+});
+
+phina.main(function() {
+  const app = GameApp({
+    startLabel: "タイトル",
+    assets: ASSETS,
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+    fit: false,
+    scenes: myscenes
+  });
+  app.run();
+});
---
+++ jam0302/main2.js
@@ -0,0 +1,227 @@
+/* global phina GameApp */
+
+phina.globalize();
+
+const SCREEN_WIDTH = 600;
+const SCREEN_HEIGHT = 480;
+
+const ASSETS = {
+  image: {
+    "buri": "./buri.png",
+    "ika": "./ika.png",
+    "maguro": "./maguro.png",
+    "samon": "./samon.png"
+  }
+};
+
+const myscenes = [
+  {
+    label: "タイトル",
+    className: "Title",
+    nextLabel: "メイン"
+  },
+  {
+    label: "メイン",
+    className: "Main",
+    nextLabel: "リザルト"
+  },
+  {
+    label: "リザルト",
+    className: "Result",
+    nextLabel: "タイトル"
+  }
+
+];
+
+phina.define("Title", {
+  superClass: "DisplayScene",
+
+  init: function() {
+    this.superInit();
+    this.backgroundColor = "skyblue";
+    const label = Label({
+      text: "すしを流すゲーム\n上下左右キーに対応したすしを流すよ\n\nEnter to start",
+      fontSize: 40,
+      fill: "white"
+    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(4));
+  },
+  update: function(app) {
+    if (app.keyboard.getKey("enter")) {
+      this.exit();
+    }
+  }
+});
+
+phina.define("Result", {
+  superClass: "DisplayScene",
+
+  init: function(param) {
+    this.superInit();
+    this.score = param.score;
+    this.backgroundColor = "skyblue";
+    /* const ground = Sprite("ground").addChildTo(this);
+    ground.origin.set(0, 0);
+    ground.width = SCREEN_WIDTH;
+    ground.height = SCREEN_HEIGHT; */
+
+    Label({
+      text: "結果発表\n" + param.score + "対27\n" +
+      (param.score >= 28 ? "優勝です！" : (param.score === 27 ? "延長戦で負けました" : "敗退です……")) +
+      "\n\n press T to tweet.",
+      fontSize: 50,
+      fill: "white"
+    }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(4));
+  },
+  update: function(app) {
+    if (app.keyboard.getKey("T")) {
+      let result = "甲子園の決勝戦は" + this.score + "対27 でした！" + " https://hukuda222.github.io/gamejam/jam0617/ ";
+      location.href = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(result) + "&hashtags=traP3jam";
+    } else if (app.keyboard.getKey("enter")) {
+      this.exit();
+    }
+  }
+});
+
+phina.define("Main", {
+  superClass: "DisplayScene",
+
+  init: function() {
+    this.superInit();
+    this.score = 20;
+    this.nageru = true;
+    this.hit = false;
+    this.rest = 3;
+    const ground = Sprite("ground").addChildTo(this);
+    ground.origin.set(0, 0);
+    ground.width = SCREEN_WIDTH;
+    ground.height = SCREEN_HEIGHT;
+
+    const picher = Sprite("picher").addChildTo(this);
+    picher.x = 300;
+    picher.y = 250;
+    picher.width = 80;
+    picher.height = 100;
+
+    const batter = Sprite("batter").addChildTo(this);
+    batter.x = 210;
+    batter.y = 340;
+    batter.width = 100;
+    batter.height = 120;
+
+    this.target = Sprite("target").addChildTo(this);
+
+    this.target.x = 280;
+    this.target.y = 350;
+    this.target.width = 75;
+    this.target.height = 100;
+
+    this.player = DisplayElement().addChildTo(this);
+    // this.target.addChildTo(this.player);
+    batter.addChildTo(this.player);
+
+    Label({
+      text: "27",
+      fontsize: 15,
+      x: 250,
+      y: 170,
+      fill: "white"
+    }).addChildTo(this);
+
+    this.ten = Label({
+      text: "20",
+      fontsize: 15,
+      x: 350,
+      y: 170,
+      fill: "white"
+    });
+    this.ten.addChildTo(this);
+    this.nokori = Label({
+      text: "3",
+      fontsize: 15,
+      x: 300,
+      y: 170,
+      fill: "white"
+    });
+    this.nokori.addChildTo(this);
+
+    this.ball = Sprite("ball").addChildTo(this);
+    this.ball.x = 300;
+    this.ball.y = 200;
+    this.ball.height = 10;
+    this.ball.width = 10;
+    this.ball.tweener.wait(1000);
+    // this.ball.tweener.to({x: 300, y: 600, width: 50, height: 50}, 4000, "swing").play();
+    // this.ball.tweener.to({x: 250, y: 250, width: 25, height: 25}, 500)
+    // .to({x: 300, y: 600, width: 50, height: 50}, 800).play().wait(1000).call((_) => {
+    // });
+    // this.ball.tweener.to({x: 300, y: 250, width: 25, height: 25}, 500, "swing")
+    //  .to({x: 300, y: 600, width: 50, height: 50}, 1000, "swing").play();
+  },
+
+  update: function(app) {
+    if (this.nageru) {
+      this.nageru = false;
+      this.hit = false;
+
+      const rand = Math.random() + (this.score - 20) * 0.05;
+      if (rand < 0.5) {
+        this.ball.tweener.set({x: 300, y: 200, height: 10, width: 10})
+          .to({x: 300, y: 600, width: 50, height: 50}, 500 + Math.random() * 1000, "swing").play();
+      } else if (rand < 0.75) {
+        this.ball.tweener.set({x: 300, y: 200, height: 10, width: 10})
+          .to({x: 250 - Math.random() * 300, y: 250 + Math.random() * 50, width: 25, height: 25}, 500)
+          .to({x: 300 + Math.random() * 50, y: 600, width: 50, height: 50}, 800, "swing").play();
+      } else if (rand < 1) {
+        this.ball.tweener.set({x: 300, y: 200, height: 10, width: 10})
+          .to({x: 350 + Math.random() * 300, y: 250 + Math.random() * 50, width: 25, height: 25}, 500)
+          .to({x: 300 - Math.random() * 50, y: 600, width: 50, height: 50}, 800, "swing")
+          .play();
+      } else {
+        this.ball.tweener.set({x: 300, y: 200, height: 10, width: 10})
+          .to({x: 250 - Math.random() * 200, y: 250 + Math.random() * 5, width: 25, height: 25}, 200)
+          .to({x: 350 + Math.random() * 200, y: 300 + Math.random() * 5, width: 25, height: 25}, 200)
+          .to({x: 250 - Math.random() * 200, y: 350 + Math.random() * 5, width: 25, height: 25}, 200)
+          .to({x: 400 + Math.random() * 100, y: 600, width: 50, height: 50}, 400)
+          .play();
+      }
+      this.ball.tweener.call((_) => {
+        this.rest -= 1;
+        this.nokori.text = this.rest;
+        if (this.rest < 0) this.exit({score: this.score});
+      }).wait(2000).call((_) => {
+        this.nageru = true;
+      });
+    }
+    var keyboard = app.keyboard;
+    // 左右移動
+    if (keyboard.getKey("left")) {
+      this.player.x -= 8;
+      this.target.x -= 8;
+    }
+    if (keyboard.getKey("right")) {
+      this.player.x += 8;
+      this.target.x += 8;
+    }
+    // 上下移動
+    if (keyboard.getKey("up")) {
+      this.player.y -= 8;
+      this.target.y -= 8;
+    }
+    if (keyboard.getKey("down")) {
+      this.player.y += 8;
+      this.target.y += 8;
+    }
+  }
+});
+
+phina.main(function() {
+  const app = GameApp({
+    startLabel: "タイトル",
+    assets: ASSETS,
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+    fit: false,
+    scenes: myscenes
+  });
+  app.run();
+});
